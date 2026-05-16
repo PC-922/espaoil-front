d94caeecce0e71bc709b2e5e252001e9cc194b26
Raúl Padilla <raul.padilla@leanmind.es>
2026-05-16 22:38:37 +0100

    fix: update API proxy target for local server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
         proxy: {
           '/api': {
-            target: 'espaoilserver-v7pg66dplldqk7srhg6hemdr:8080',
+            target: 'https://espaoilserver.padyserveroracle.duckdns.org',
             changeOrigin: true,
             secure: true,
             rewrite: (path: string) => path.replace(/^\/api/, ''),
